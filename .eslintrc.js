19910e9c68c8e9cdb0aad76f90dd1116a9733a7b
bmviniciuss <bmvinicius11@gmail.com>
2021-09-01 23:08:47 -0300

    chore(#1): add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers',
+    'jest'
+  ],
+  rules: {
+    'no-useless-constructor': 0,
+    'import-helpers/order-imports': [
+      'error',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          'module',
+          [
+            'parent',
+            'sibling',
+            'index'
+          ]
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true
+        }
+      }
+    ]
+  }
+}
